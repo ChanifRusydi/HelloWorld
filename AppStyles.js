fb5718974b1b17867708a5b5fb4d7d41c158a30c
ChanifRusydi <62838892+ChanifRusydi@users.noreply.github.com>
2021-10-14 18:23:36 +0700

    AppStyles

---
+++ AppStyles.js
@@ -0,0 +1,10 @@
+import { StyleSheet } from "react-native"
+export default StyleSheet.create({
+    text_big:
+    {
+        fontWeight :'bold',fontSize:32
+    },
+    container:(
+        flex:1, justifyContent:"center", alignItems:"center"
+    )
+})
