61dced579cf276e71183899528b8a53a390268e9
Chanif Rusydi <chanif.rusydi@gmail.com>
2021-10-14 17:29:59 +0700

    back to custom component app

---
+++ AppCustomComponent.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {Text, View, Image} from 'react-native';
+const AppHeader = () => {
+  return (
+    <View style={{height: 60, backgroundColor: 'gray'}}>
+      <Text style={{fontWeight: 'bold', fontSize: 32}}>Application Title</Text>
+    </View>
+  );
+};
+
+const AppContent = () => {
+  return (
+    <View>
+      <Text style={{fontWeight: 'bold', fontSize: 23}}>Content</Text>
+      <Image
+        source={require('./images/index.png')}
+        style={{width: 300, height: 50, resizeMode: 'contain'}}
+      />
+    </View>
+  );
+};
