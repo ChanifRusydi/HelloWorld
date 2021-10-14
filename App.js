e47509977a02c23d6c87aedefa7ff23ecb09ad3c
Chanif Rusydi <chanif.rusydi@gmail.com>
2021-10-14 18:05:25 +0700

    inline style

--- App.js
+++ App.js
@@ -1,13 +1,23 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import {Text, View} from 'react-native';
 
-const App = () =>
-{
-  return(
-    <Views style ={{
-      flex: 1, justify
-    }}
-  )
-}
+const App = () => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
+      <Text
+        style={{
+          fontWeight: 'bold',
+          fontSize: 32,
+        }}>
+        Muchammad 'Irfan Chanif Rusydi 1103190001
+      </Text>
+    </View>
+  );
+};
 
 export default App;
