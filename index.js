8da41287bf895415f61628eb094c165227bfbb07
ChanifRusydi <62838892+ChanifRusydi@users.noreply.github.com>
2021-12-23 21:08:25 +0700

    trying on physical devices

--- index.js
+++ index.js
@@ -3,7 +3,9 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
+// import App from './App';
+// import AppJustifyContent from './AppScrollView';
+import AppScrollView from './AppScrollView';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => AppScrollView);
